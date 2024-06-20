9c903e7d0f584f489f9a80e662ad5ccb330e22ff
JulianMessner <julianmessner.13@gmail.com>
2024-06-20 20:26:48 +0200

    authenticate

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from .models import Chat, Message
 
-# Create your views here.
 def index(request):
-    return render(request, 'chat/index.html', {'username': 'Julian'})
+    if request.method == 'POST':
+        print('Received data ' + request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages': chatMessages})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/chat')
+        else:
+            return render(request, 'auth/login.html', {'wrongPassword': True})
+    return render(request, 'auth/login.html')
